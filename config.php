eca42e9d4fd8de29182d425ba90335cb28b2effa
tathuma <stathuma@gmail.com>
2022-01-18 23:45:17 +0900

    ファイルの紐付け

--- config.php
+++ config.php
@@ -1,2 +1,7 @@
 <!-- 定数ファイル -->
+<?php  
+$uri = $_SERVER['REQUEST_URI'];
 
+define('BASE_PASS', $uri);
+define('BASE_CSS_PASS', BASE_PASS . 'css/');
+define('BASE_JS_PASS', BASE_PASS . 'js/');
--- index.php
+++ index.php
@@ -1,3 +1,33 @@
-<?php  
+<?php 
+require_once 'config.php';
+?>
 
-echo 'hello';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>portforio</title>
+    <link rel="stylesheet" href="<?php echo BASE_CSS_PASS ?>style.css">
+</head>
+<body>
+    <h1 class="color">
+        portforio
+    </h1>
+
+    <script src="<?php echo BASE_JS_PASS ?>"></script>
+</body>
+</html>
+
+
+<?php 
+
+try {
+
+} catch(exception) {
+
+} finally {
+
+}
+?>
